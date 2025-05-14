3d185fb050fddd07a32dc8bdcd48245347d34a8d
diogo <dvmfa90@gmail.com>
2025-05-14 13:57:44 +0100

    added whichkey shortcut

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -148,6 +148,7 @@ local mappings = {
     R = { "<cmd>Telescope registers<cr>", "Registers" },
     k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
     C = { "<cmd>Telescope commands<cr>", "Commands" },
+    T = { "<cmd>Telescope treesitter<cr>", "Treesitter" },
   },
 
   g = {
