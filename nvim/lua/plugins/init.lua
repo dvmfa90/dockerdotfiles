271845d2fa84522ab2bba8857e5518b31bb0eccc
diogo <dvmfa90@gmail.com>
2025-10-08 11:46:52 +0100

    removing noiceP
    being replaced by snacks

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -88,17 +88,17 @@ return {
     opts = {}, 
     dependencies = { "nvim-tree/nvim-web-devicons" } -- Bufferline usually needs devicons
   },
-{
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-  },
-  config = function()
-    require("config.noice")
-  end,
-},
+-- {
+--   "folke/noice.nvim",
+--   event = "VeryLazy",
+--   dependencies = {
+--       "MunifTanjim/nui.nvim",
+--       "rcarriga/nvim-notify",
+--   },
+--   config = function()
+--     require("config.noice")
+--   end,
+-- },
  {
    "nvim-treesitter/nvim-treesitter",
     event = "VeryLazy",
