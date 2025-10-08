1c1bb60eaba6d675744d8d9160f72a1315409625
diogo <dvmfa90@gmail.com>
2025-10-08 13:37:07 +0100

    quick commit

--- nvim/lua/config/whichkey.lua
+++ nvim/lua/config/whichkey.lua
@@ -10,87 +10,83 @@ wk.setup({})
 wk.add({
   mode = "n",
   {
-    -- ----------------------------------------------------------------------
-  -- Buffer local Keymaps
-  -- ----------------------------------------------------------------------
-    -- Mapped key
-    "<leader>?",
-    -- Action: Function to show only buffer-local keymaps
-    function()
-      wk.show({ scope = "buffer" })
-    end,
-    desc = "Buffer Local Keymaps (which-key)",
+    -- Top Pickers & Explorer
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    -- find
+    { "<leader>f", group = "Find" }, -- 💡 CORRECTED: Prefix added
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+    -- git
+    { "<leader>g", group = "GIT" }, -- 💡 CORRECTED: Prefix added
+    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+    { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+    { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    -- Grep
+    { "<leader>s", group = "Search" }, -- 💡 CORRECTED: Prefix added
+    { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+    { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+    { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    -- search
+    { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+    { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
+    { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+    { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+    { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+    { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+    { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+    { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+    { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+    { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+    { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
+    { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+    { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+    { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+    -- LSP
+    -- { "<leader>s", group = "Search" }, -- 💡 CORRECTED: Prefix added
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+    -- Other
+    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+    { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+    { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
 
-      -- ----------------------------------------------------------------------
-  -- B. Buffers Group
-  -- ----------------------------------------------------------------------
-  { "<leader>b", group = "Buffers" }, -- 💡 CORRECTED: Prefix added
-  
-  -- Buffer Commands
-  { "<leader>bk", "<cmd>bd<cr>", desc = "Kill Buffer" },
-
-  -- ----------------------------------------------------------------------
-  -- C. Debug Group
-  -- ----------------------------------------------------------------------
-  { "<leader>d", group = "Debug" }, -- 💡 CORRECTED: Prefix added
-
-  -- DAP Commands
-  { "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", desc = "Breakpoint" },
-  { "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", desc = "Continue" },
-  { "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", desc = "Into" },
-  { "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", desc = "Over" },
-  { "<leader>dO", "<cmd>lua require'dap'.step_out()<cr>", desc = "Out" },
-  { "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", desc = "Repl" },
-  { "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", desc = "Last" },
-  { "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", desc = "UI" },
-  { "<leader>dx", "<cmd>lua require'dap'.terminate()<cr>", desc = "Exit" },
-
-{ "<leader>l", group = "LSP" }, -- Group definition
-
--- LSP Actions (These correspond to your <leader> maps)
-{ "<leader>lca", vim.lsp.buf.code_action, desc = "Code Action (Lightbulb)" },
-{ "<leader>lrn", vim.lsp.buf.rename, desc = "Rename Symbol" },
-
--- LSP Navigation (These are your 'g' maps, categorized under <leader>l)
-{ "<leader>ld", vim.lsp.buf.definition, desc = "Go to Definition" },
-{ "<leader>lD", vim.lsp.buf.declaration, desc = "Go to Declaration" },
-{ "<leader>lgr", "<cmd>Telescope lsp_references<CR>", desc = "Go to References" },
-{ "<leader>li", vim.lsp.buf.implementation, desc = "Go to Implementation" },
-  -- ----------------------------------------------------------------------
-  -- D. File Group 
-  -- ----------------------------------------------------------------------
-  { "<leader>f", group = "File" }, -- 💡 CORRECTED: Prefix added
-  
-  -- File Commands
-  { "<leader>fb", "<cmd>Telescope file_browser<cr>", desc = "Browse files" },
-  { "<leader>fd", "<cmd>windo diffthis<cr>", desc = "Diff splits" },
-
-
-  -- ----------------------------------------------------------------------
-  -- E. Search Group (Telescope)
-  -- ----------------------------------------------------------------------
-  { "<leader>s", group = "Search" }, -- ✅ CORRECTED: Prefix added
-  
-  -- Telescope Commands
-  { "<leader>sB", "<cmd>Telescope buffers<cr>", desc = "Search Buffers" },
-  { "<leader>sb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch" },
-  { "<leader>sc", "<cmd>Telescope colorscheme<cr>", desc = "Colorscheme" },
-  { "<leader>sd", "<cmd>Telescope diagnostics <cr>", desc = "Diagnostics" },
-  { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-  { "<leader>st", "<cmd>Telescope live_grep<cr>", desc = "Find Text" },
-  { "<leader>sS", "<cmd>Snippets<cr>", desc = "Search Snippets" },
-  { "<leader>ss", "<cmd>Telescope grep_string<cr>", desc = "Find String" },
-  { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
-  { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Highlights" },
-  { "<leader>si", "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", desc = "Media" },
-  { "<leader>sl", "<cmd>Telescope resume<cr>", desc = "Last Search" },
-  { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-  { "<leader>sr", "<cmd>Telescope oldfiles<cr>", desc = "Recent File" },
-  { "<leader>sR", "<cmd>Telescope registers<cr>", desc = "Registers" },
-  { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
-  { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-  { "<leader>sT", "<cmd>Telescope treesitter<cr>", desc = "Treesitter" },
-
-}
-
+  }
 })
+
